8620ac83eeea4dd4c8493b85c5f7398f334b9010
Domoskanonos <Tgg8855tt#>
2021-04-29 07:43:17 +0200

    modified

--- rasp4binstall.sh
+++ rasp4binstall.sh
@@ -12,16 +12,16 @@ sudo apt install -y python3-pip
 sudo apt install -y python-dev
 python3 -m pip install numpy
 
+# install python-virtualenv
+sudo apt install python-virtualenv
+virtualenv --system-site-packages ~/env
+source ~/env/bin/activate
+
 # install respeaker driver
 git clone https://github.com/HinTak/seeed-voicecard.git
 cd seeed-voicecard
 sudo ./install.sh
 cd ..
-
-# install python-virtualenv
-sudo apt install python-virtualenv
-virtualenv --system-site-packages ~/env
-source ~/env/bin/activate
 
 # install respeaker led driver
 git clone https://github.com/respeaker/4mics_hat.git
