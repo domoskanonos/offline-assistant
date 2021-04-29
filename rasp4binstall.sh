effd004c92af0a767c81909d700d20027c21fab2
domoskanonos <tgz014vb>
2021-04-29 20:06:20 +0200

    Anpassungen

--- rasp4binstall.sh
+++ rasp4binstall.sh
@@ -8,12 +8,7 @@ sudo apt install -y \
 sudo apt install -y git
 
 # clone domoskanonos offline-server repo and copy root resources
-_OFFLINE_SERVER_DIR="~/offline-assistant/"
-if [ -d "$_OFFLINE_SERVER_DIR" ]; then
-  echo "Installing offline-server in ${_OFFLINE_SERVER
-_DIR}..."
-  git clone https://github.com/domoskanonos/offline-assistant.git
-fi
+git clone https://github.com/domoskanonos/offline-assistant.git
 cd offline-assistant
 sudo cp -r ./root/home /
 sudo cp -r ./root/var /
@@ -34,11 +29,7 @@ sudo ./install.sh
 cd ..
 
 # install respeaker led driver
-_4MIC_DIR="~/4mics_hat/"
-if [ -d "$_4MIC_DIR" ]; then
-  echo "Installing 4MIC LED files in ${_4MIC_DIR}..."
-  git clone https://github.com/respeaker/4mics_hat.git
-fi
+git clone https://github.com/respeaker/4mics_hat.git
 cd 4mics_hat
 source ~/env/bin/activate
 pip install numpy spidev gpiozero
